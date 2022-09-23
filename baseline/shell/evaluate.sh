593761131024b1eea9dd636eee1d134db8d0544f
Jiuming Qin <77429740+holajoa@users.noreply.github.com>
2022-09-23 17:00:23 +0100

    debug configs

--- baseline/shell/evaluate.sh
+++ baseline/shell/evaluate.sh
@@ -1,1 +1,9 @@
-python evaluate.py --test ../data/dev/dev.txt --out_dir ./trained_model --model_name roberta_squad2_final --gpus 1 --encoder_model deepset/roberta-base-squad2 --batch_size 32 --model .\trained_model\roberta_squad2_lr_2e-5\lightning_logs\version_0 --max_length 128
+python evaluate.py \
+    --test ../data/dev/dev.txt \
+    --out_dir ./trained_model \
+    --model_name roberta_squad2_final \
+    --gpus 1 \
+    --encoder_model deepset/roberta-base-squad2 \
+    --batch_size 32 \
+    --model ./trained_model/roberta_squad2_lr_2e-5/lightning_logs/version_0 \
+    --max_length 200
